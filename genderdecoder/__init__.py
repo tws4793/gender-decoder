e6d3bd7cae4198a358a8953bd6865b8673568ee6
Teo Wei Shen <tws4793@gmail.com>
2020-04-09 17:16:18 +0800

    Make code work

--- genderdecoder/__init__.py
+++ genderdecoder/__init__.py
@@ -1,3 +1,1 @@
-from assess import assess
-from wordlists import feminine_coded_words
-from wordlists import masculine_coded_words
+from .combine import *
--- genderdecoder/combine.py
+++ genderdecoder/combine.py
@@ -1,40 +1,53 @@
 import re
-import wordlists
+
+feminine_file = 'wordlist/feminine.txt'
+masculine_file = 'wordlist/masculine.txt'
+
+def get_words(is_feminine = False):
+    words = []
+
+    with open(masculine_file if is_feminine else feminine_file, 'r') as f:
+        words = f.read().split('\n')
+    
+    return words
 
 def assess(ad_text):
+    masculine_coded_words = get_words(True)
+    feminine_coded_words = get_words(False)
+
     ad_text = ''.join([i if ord(i) < 128 else ' ' for i in ad_text])
     ad_text = re.sub("[\\s]", " ", ad_text, 0, 0)
-    ad_text = re.sub("[\.\t\,\:;\(\)\.]", "", ad_text, 0, 0).split(" ")
+    ad_text = re.sub(r'[\.\t\,\:;\(\)\.]', "", ad_text, 0, 0).split(" ")
     ad_text = [ad for ad in ad_text if ad != ""]
-        
+
     masculine_coded_words = [adword for adword in ad_text
-        for word in wordlists.masculine_coded_words
+        for word in masculine_coded_words
         if adword.startswith(word)]
-    
+
     feminine_coded_words = [adword for adword in ad_text
-        for word in wordlists.feminine_coded_words
+        for word in feminine_coded_words
         if adword.startswith(word)]
-    
+
     if feminine_coded_words and not masculine_coded_words:
         result = "strongly feminine-coded"
     elif masculine_coded_words and not feminine_coded_words:
         result = "strongly masculine-coded"
     elif not masculine_coded_words and not feminine_coded_words:
         result = "neutral"
-    else: 
+    else:
         if len(feminine_coded_words) == len(masculine_coded_words):
             result = "neutral"
-        if ((len(feminine_coded_words) / len(masculine_coded_words)) >= 2 and 
-            len(feminine_coded_words) > 5):
+        if ((len(feminine_coded_words) / len(masculine_coded_words)) >= 2 and
+                len(feminine_coded_words) > 5):
             result = "strongly feminine-coded"
-        if ((len(masculine_coded_words) / len(feminine_coded_words)) >= 2 and 
-            len(masculine_coded_words) > 5):
+        if ((len(masculine_coded_words) / len(feminine_coded_words)) >= 2 and
+                len(masculine_coded_words) > 5):
             result = "strongly masculine-coded"
         if len(feminine_coded_words) > len(masculine_coded_words):
             result = "feminine-coded"
         if len(masculine_coded_words) > len(feminine_coded_words):
             result = "masculine-coded"
-    
+
     if "feminine" in result:
         explanation = ("This job ad uses more words that are stereotypically feminine "
             "than words that are stereotypically masculine. Fortunately, the research "
