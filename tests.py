617329cdd87e7df20a1650327d184d744635733f
Teo Wei Shen <tws4793@gmail.com>
2020-04-09 17:16:50 +0800

    Make test case presentable

--- tests.py
+++ tests.py
@@ -2,9 +2,13 @@ import unittest
 import genderdecoder
 
 class TestPackage(unittest.TestCase):
-
   def test_assess(self):
-      assert genderdecoder.assess('test') == {'explanation': "This job ad doesn't use any words that are stereotypically masculine and stereotypically feminine. It probably won't be off-putting to men or women applicants.", 'masculine_coded_words': [], 'result': 'neutral', 'feminine_coded_words': []}
+        assert genderdecoder.assess('test') == {
+            'explanation': "This job ad doesn't use any words that are stereotypically masculine and stereotypically feminine. It probably won't be off-putting to men or women applicants.", 
+            'masculine_coded_words': [], 
+            'result': 'neutral', 
+            'feminine_coded_words': []
+        }
 
 if __name__ == '__main__':
     unittest.main()
